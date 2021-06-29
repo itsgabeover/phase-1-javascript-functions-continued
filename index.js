74d7f0cfda5d6ccb8a6d3aaf34200bd7ff31d47d
itsgabeover <gabe.enrique.miranda@gmail.com>
2021-06-28 17:25:38 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+function saturdayFun(activity){
+    if (activity == null){
+        return `This Saturday, I want to roller-skate!`;
+    } else {
+        return `This Saturday, I want to ${activity}!`;
+    }
+}
+
+function mondayWork(activity){
+    if (activity == null){
+        return `This Monday, I will go to the office.`;
+    } else {
+        return `This Monday, I will ${activity}.`;
+    }
+}
+
+function wrapAdjective(specialChar = '*'){
+    return function(adjective = 'special') {
+        return `You are ${specialChar}${adjective}${specialChar}!`;
+    }
+
+}
